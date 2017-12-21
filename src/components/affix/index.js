9542f8e181021c8a45df09a98325ef899d0dc228
Pierre-Yves Tsen <hello@pierreyves.paris>
2017-12-21 16:38:26 +0100

    Change style affix

--- src/components/affix/index.js
+++ src/components/affix/index.js
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Animated } from 'react-native';
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { Animated } from "react-native";
 
-import styles from './styles';
+import styles from "./styles";
 
 export default class Affix extends PureComponent {
   static defaultProps = {
@@ -18,7 +18,7 @@ export default class Affix extends PureComponent {
     active: PropTypes.bool,
     focused: PropTypes.bool,
 
-    type: PropTypes.oneOf(['prefix', 'suffix']).isRequired,
+    type: PropTypes.oneOf(["prefix", "suffix"]).isRequired,
 
     fontSize: PropTypes.number.isRequired,
     baseColor: PropTypes.string.isRequired,
@@ -38,7 +38,7 @@ export default class Affix extends PureComponent {
     let { active, focused } = this.props;
 
     this.state = {
-      opacity: new Animated.Value((active || focused)? 1 : 0),
+      opacity: new Animated.Value(active || focused ? 1 : 0),
     };
   }
 
@@ -46,19 +46,17 @@ export default class Affix extends PureComponent {
     let { opacity } = this.state;
     let { active, focused, animationDuration } = this.props;
 
-    if ((focused ^ props.focused) || (active ^ props.active)) {
-      Animated
-        .timing(opacity, {
-          toValue: (props.active || props.focused)? 1 : 0,
-          duration: animationDuration,
-        })
-        .start();
+    if (focused ^ props.focused || active ^ props.active) {
+      Animated.timing(opacity, {
+        toValue: props.active || props.focused ? 1 : 0,
+        duration: animationDuration,
+      }).start();
     }
   }
 
   render() {
     let { opacity } = this.state;
-    let { style, children, type, fontSize, baseColor: color } = this.props;
+    let { style, children, type, fontSize } = this.props;
 
     let containerStyle = {
       height: fontSize * 1.5,
@@ -66,19 +64,18 @@ export default class Affix extends PureComponent {
     };
 
     let textStyle = {
-      color,
       fontSize,
     };
 
     switch (type) {
-      case 'prefix':
-        containerStyle.paddingRight = 8;
-        textStyle.textAlign = 'left';
+      case "prefix":
+        containerStyle.paddingRight = 4;
+        textStyle.textAlign = "left";
         break;
 
-      case 'suffix':
-        containerStyle.paddingLeft = 8;
-        textStyle.textAlign = 'right';
+      case "suffix":
+        containerStyle.paddingLeft = 4;
+        textStyle.textAlign = "right";
         break;
     }
 
